426baff62a585355a15b90f329fb91b338718af8
ureishi <victorique2020+github@gmail.com>
2020-10-20 03:55:03 +0900

    update

--- hitokoto.rb
+++ hitokoto.rb
@@ -1,4 +1,13 @@
+require 'json'
+require 'mini_magick'
+require 'net/https'
 require 'twitter'
+require 'uri'
+
+puts `ruby --version`
+
+### get image from twitter
+puts 'get image...'
 
 ENVIRONMENT = {
 	TWITTER_CONSUMER_KEY: ARGV[0],
@@ -7,83 +16,148 @@ ENVIRONMENT = {
 	TWITTER_ACCESS_TOKEN_SECRET: ARGV[3]
 }
 
-$client
 def twitter_client
-	return $client ||= Twitter::REST::Client.new{|config|
-		config.consumer_key = ENVIRONMENT[:TWITTER_CONSUMER_KEY]
-		config.consumer_secret = ENVIRONMENT[:TWITTER_CONSUMER_SECRET]
-		config.access_token = ENVIRONMENT[:TWITTER_ACCESS_TOKEN]
-		config.access_token_secret = ENVIRONMENT[:TWITTER_ACCESS_TOKEN_SECRET]
+	$client ||= Twitter::REST::Client.new{
+		_1.consumer_key = ENVIRONMENT[:TWITTER_CONSUMER_KEY]
+		_1.consumer_secret = ENVIRONMENT[:TWITTER_CONSUMER_SECRET]
+		_1.access_token = ENVIRONMENT[:TWITTER_ACCESS_TOKEN]
+		_1.access_token_secret = ENVIRONMENT[:TWITTER_ACCESS_TOKEN_SECRET]
 	}
 end
 
-def get_orig_image_uri s
-	"#{s}?name=orig"
-end
+list_url = 'https://raw.githubusercontent.com/suicai/hitokoto-list/main/list.txt'
+puts "get tweet urls from #{list_url}"
 
-query = File.open('query.txt'){_1.gets}.chomp
-puts "query: #{query}"
+tweet_urls = URI.open(list_url).read.split("\n").sample(5)
+puts "tweet_urls:\n\t#{tweet_urls.join "\n\t"}"
 
-allowlist = File.open('allowlist.txt'){|f|
-	f.each_line.map{|l| l.split.first.to_i}
+#tweet_urls[0] = "https://twitter.com/chokakuji/status/1317956366795894784"
+
+tweets = tweet_urls.map{twitter_client.status _1, tweet_mode: 'extended'}
+
+image_uris = tweets.map{"#{_1.media.first.media_uri_https}?name=orig" if _1 && _1.media?}
+puts "image_uris:\n\t#{image_uris.join "\n\t"}"
+
+### create directory
+puts 'create directory...'
+Dir.mkdir 'public' unless Dir.exists? 'public'
+puts 'created: public'
+
+### create image
+puts 'create hitokoto.jpg (for forward compatibility)'
+SIZE_LEGACY = '1920x1080'
+MiniMagick::Tool::Convert.new{
+	_1.size SIZE_LEGACY
+	_1 << 'canvas:#2f2725'
+	_1 << 'base_legacy.jpg'
 }
-puts "allowlist: #{allowlist}"
 
-N = 50
+MiniMagick::Image
+.open('base_legacy.jpg')
+.composite(
+	MiniMagick::Image
+	.open(image_uris.first || 'default.png')
+	.resize(SIZE_LEGACY)
+){
+	_1.compose 'Over'
+	_1.gravity 'Center'
+	_1.geometry "+0+0"
+}
+.write('public/hitokoto.jpg')
+puts 'created: hitokoto.jpg'
 
-result_tweets = twitter_client.search(
-	query,
-	count: N,
-	result_type: 'recent',
-)
+puts 'create images...'
+BASE_W = 1024
+BASE_H = 1448
+SIZE = 1000
+NEW_ICON_X = [-146, -48, 0, 50, 148]
+FONT_SERIF = '.font/Noto_Serif_JP/NotoSerifJP-Regular.otf'
+FONT_SANS = '.font/Noto_Sans_JP/NotoSansJP-Regular.otf'
 
-puts
+MiniMagick::Tool::Convert.new{
+	_1.size "#{BASE_W}x#{BASE_H}"
+	_1 << 'canvas:#2f2725'
+	_1 << 'base.png'
+}
 
-auto_mode = true
+image = MiniMagick::Image
+.open('base.png')
+.combine_options{
+	pos = '0, 0'
+	text = '今日のことば'
+	_1.font FONT_SERIF
+	_1.fill '#fcfcfc'
+	_1.gravity 'North'
+	_1.pointsize 125
+	_1.draw "text #{pos} '#{text}'"
+}
+.combine_options{
+	margin = 3
+	pos_nw = "#{margin}, #{BASE_H - 120 + margin}"
+	pos_se = "#{BASE_W - margin}, #{BASE_H - margin}"
+	_1.fill '#0d3e59'
+	_1.draw "rectangle #{pos_nw} #{pos_se}"
+}
 
-image_uri = nil
+image.write 'base.png'
+puts 'created: base.png'
 
-if auto_mode
-	result_tweets.take(N).each{|tw|
-		if allowlist.include? tw.user.id #or true
-			sleep 10
-			t = (if tw.retweet?
-				tw.retweeted_status
-			else
-				twitter_client.status(tw, tweet_mode: 'extended')
-			end)
-
-			if t.media?
-				image_uri = get_orig_image_uri t.media.first.media_uri_https
-			elsif t.uris.length > 0
-				t.uris.each{
-					if _1.expanded_url.to_s.start_with? 'https://twitter.com'
-						sleep 5
-						t = twitter_client.status(_1.expanded_url, tweet_mode: 'extended')
-						if t.media?
-							image_uri = get_orig_image_uri t.media.first.media_uri_https
-						end
-					end
-					break if image_uri
-				}
-			end
-		end
-
-		break if image_uri
+tweets.length.times{|p|
+	image = MiniMagick::Image
+	.open('base.png')
+	.combine_options{
+		pos = '0, 152'
+		text = "#{tweets[p].user.name}"[..30]
+		_1.font FONT_SERIF
+		_1.fill '#fcfcfc'
+		_1.gravity 'North'
+		_1.pointsize 30
+		_1.draw "text #{pos} '#{text}'"
 	}
-else
-	sleep 5
-	t = twitter_client.status(File.open('manual_url.txt'){_1.gets}.chomp, tweet_mode: 'extended')
-	if t.media?
-		image_uri = get_orig_image_uri t.media.first.media_uri_https
-	end
-end
+	.combine_options{
+		pos = '0, 186'
+		text = "@#{tweets[p].user.screen_name}"
+		_1.font FONT_SANS
+		_1.fill '#fcfcfc'
+		_1.gravity 'North'
+		_1.pointsize 25
+		_1.draw "text #{pos} '#{text}'"
+	}
+	.combine_options{
+		pos = '5, 117'
+		text = "※写真の権利は撮影者に帰属します。\n※宗派を問わず気まぐれに更新されます。\n※営利・勧誘目的ではありません。"
+		_1.font FONT_SERIF
+		_1.fill '#fcfcfc'
+		_1.gravity 'SouthWest'
+		_1.pointsize 20
+		_1.draw "text #{pos} '#{text}'"
+	}
+	.combine_options{
+		pos = '5, 117'
+		text = "更新管理者Discord: すいかい#9599"
+		_1.font FONT_SANS
+		_1.fill '#fcfcfc'
+		_1.gravity 'SouthEast'
+		_1.pointsize 20
+		_1.draw "text #{pos} '#{text}'"
+	}
+	.composite(
+		MiniMagick::Image
+		.open(image_uris[p] || 'default.png')
+		.resize("#{SIZE}x#{SIZE}")
+	){
+		_1.compose 'Over'
+		_1.gravity 'Center'
+		_1.geometry "+0+0"
+	}
+	
+	image.write "public/hitokoto#{p}.png"
+	puts "created: hitokoto#{p}.png"
+}
 
-image_uri ||= 'https://ureishi.github.io/hitokoto/hitokoto.jpg'
+### optimize
+puts "optimize"
+`pngquant --force --ext .png --speed 1 public/hitokoto*.png`
+`advpng -z -4 -i 10 public/hitokoto*.png`
 
-puts "image_uri: #{image_uri}"
-
-file_name = 'hitokoto.jpg'
-# 試行回数3回 タイムアップ3秒 待機時間3秒
-command = "wget -t 3 -T 3 -w 3 -O #{file_name} #{image_uri}"
-`#{command}`
+puts "finished"
